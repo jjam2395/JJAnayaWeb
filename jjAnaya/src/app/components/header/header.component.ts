45578cd459561ca773fc8a6c589e2de7a2805d4f
Juan Anaya <jjam2395@gmail.com>
2018-03-21 14:02:53 -0600

    cambios pequeños para SEO -version stable

--- jjAnaya/src/app/components/header/header.component.ts
+++ jjAnaya/src/app/components/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-//import { AngularSvgIconModule } from 'angular-svg-icon';
-//import * as waypoints from '../jquery.waypoints.min.js';
 declare var  JQuery:any;
 declare var $:any;
 
